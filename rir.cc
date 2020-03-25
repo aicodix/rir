b9c367a75a1c2d6c18e5c7aa8e2b67aa6fd002a9
Ahmet Inan <xdsopl@gmail.com>
2020-03-25 13:12:38 +0100

    support both repeated and multiple sequences

--- rir.cc
+++ rir.cc
@@ -11,7 +11,7 @@ Copyright 2020 Ahmet Inan <inan@aicodix.de>
 #include "wav.hh"
 
 template <typename cmplx, int filter_length>
-struct Compute
+struct MultipleSequences
 {
 	static const int kernel_length = filter_length / 2;
 	const int filter_count;
@@ -22,7 +22,7 @@ struct Compute
 	cmplx fdom[kernel_length];
 	cmplx tmp[kernel_length];
 	cmplx avg[kernel_length];
-	Compute(DSP::WriteWAV<value> &output_file, DSP::ReadWAV<value> &input_file, DSP::ReadWAV<value> &filter_file, int clip_length) : filter_count(filter_file.channels())
+	MultipleSequences(DSP::WriteWAV<value> &output_file, DSP::ReadWAV<value> &input_file, DSP::ReadWAV<value> &filter_file, int clip_length) : filter_count(filter_file.channels())
 	{
 		value *frame = new value[filter_count];
 		value *filter = new value[filter_length*filter_count];
@@ -111,6 +111,60 @@ struct Compute
 	}
 };
 
+template <typename cmplx, int filter_length>
+struct RepeatedSequence
+{
+	static const int kernel_length = filter_length / 2;
+	typedef typename cmplx::value_type value;
+	DSP::RealToHalfComplexTransform<filter_length, cmplx> fwd;
+	DSP::FastFourierTransform<kernel_length, cmplx, 1> bwd;
+	value input[filter_length];
+	cmplx kernel[kernel_length];
+	cmplx tmp[kernel_length];
+	cmplx rir[kernel_length];
+	cmplx avg[kernel_length];
+	RepeatedSequence(DSP::WriteWAV<value> &output_file, DSP::ReadWAV<value> &input_file, DSP::ReadWAV<value> &filter_file, int clip_length)
+	{
+		filter_file.read(input, filter_length);
+		fwd(kernel, input);
+		for (int i = 0; i < kernel_length; ++i)
+			kernel[i] = conj(kernel[i]);
+		for (int i = 0; i < kernel_length; ++i)
+			kernel[i] /= value(filter_length) * sqrt(kernel_length);
+		input_file.skip(filter_length / 2);
+		input_file.read(input, filter_length);
+		while (input_file.good()) {
+			fwd(tmp, input);
+			for (int i = 0; i < kernel_length; ++i)
+				tmp[i] *= kernel[i];
+			bwd(rir, tmp);
+			for (int i = 0; i < kernel_length; ++i)
+				avg[i] += rir[i];
+			input_file.read(input, filter_length);
+		}
+		cmplx val_max;
+		value abs_max = 0;
+		int max_pos = 0;
+		for (int i = 0; i < kernel_length; ++i) {
+			if (abs_max < abs(avg[i])) {
+				val_max = avg[i];
+				abs_max = abs(avg[i]);
+				max_pos = i;
+			}
+		}
+		for (int i = 0; i < kernel_length; ++i)
+			avg[i] /= val_max;
+
+		if (clip_length < 0)
+			clip_length = kernel_length;
+		int shift = (clip_length-1)/2;
+		int offset = (max_pos-shift+kernel_length)%kernel_length;
+		int rest = kernel_length-offset;
+		output_file.write(reinterpret_cast<value *>(avg+offset), std::min(clip_length, rest), 2);
+		output_file.write(reinterpret_cast<value *>(avg), std::max(0, clip_length-rest), 2);
+	}
+};
+
 int main(int argc, char **argv)
 {
 	if (argc != 5) {
@@ -141,18 +195,32 @@ int main(int argc, char **argv)
 
 	DSP::WriteWAV<value> output_file(output_name, input_file.rate() / 2, input_file.bits(), 2);
 
+	bool multiple = filter_file.channels() > 1;
+
 	switch (filter_file.frames()) {
 	case 16384:
-		new Compute<cmplx, 16384>(output_file, input_file, filter_file, clip_length);
+		if (multiple)
+			new MultipleSequences<cmplx, 16384>(output_file, input_file, filter_file, clip_length);
+		else
+			new RepeatedSequence<cmplx, 16384>(output_file, input_file, filter_file, clip_length);
 		break;
 	case 65536:
-		new Compute<cmplx, 65536>(output_file, input_file, filter_file, clip_length);
+		if (multiple)
+			new MultipleSequences<cmplx, 65536>(output_file, input_file, filter_file, clip_length);
+		else
+			new RepeatedSequence<cmplx, 65536>(output_file, input_file, filter_file, clip_length);
 		break;
 	case 262144:
-		new Compute<cmplx, 262144>(output_file, input_file, filter_file, clip_length);
+		if (multiple)
+			new MultipleSequences<cmplx, 262144>(output_file, input_file, filter_file, clip_length);
+		else
+			new RepeatedSequence<cmplx, 262144>(output_file, input_file, filter_file, clip_length);
 		break;
 	case 1048576:
-		new Compute<cmplx, 1048576>(output_file, input_file, filter_file, clip_length);
+		if (multiple)
+			new MultipleSequences<cmplx, 1048576>(output_file, input_file, filter_file, clip_length);
+		else
+			new RepeatedSequence<cmplx, 1048576>(output_file, input_file, filter_file, clip_length);
 		break;
 	default:
 		std::cerr << "Unsupported filter length." << std::endl;
